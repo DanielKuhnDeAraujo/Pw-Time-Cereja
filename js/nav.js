48f8c77de4f7c516d6893eba51fc21b1fa8f9ad6
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-09-04 08:53:02 -0300

    menu

---
+++ js/nav.js
@@ -0,0 +1,36 @@
+document.body.innerHTML+='\
+<nav class="navbar navbar-expand-lg fixed-top">\
+        <div class="container-fluid">\
+            <a class="navbar-brand ms-3" href="index.html">\
+                <img src="img/teamcreja.png"  height="60px"alt="">\
+                Team Cherry\
+            </a>\
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+            </button>\
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5" >\
+                    <li class="nav-item">\
+                    <a class="nav-link " aria-current="page" href="index.html">Home</a>\
+                    </li>\
+                    <li class="nav-item">\
+                    <a class="nav-link" href="sobre.hmtl">Sobre Nós</a>\
+                    </li>\
+                    <li class="nav-item">\
+                    <a class="nav-link" href="loja.htlm">Loja</a>\
+                    </li>\
+                    <li class="nav-item dropdown me-3">\
+                    <a class="nav-link dropdown-toggle" id="drop" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">\
+                        Jogos\
+                    </a>\
+                    <ul class="dropdown-menu">\
+                        <li><a class="dropdown-item" href="silksong.html">Silksong</a></li>\
+                        <li><a class="dropdown-item" href="hk.html">Hollow Knight</a></li>\
+                        <li><hr class="dropdown-divider"></li>\
+                        <li><a class="dropdown-item" href="hungry.html">Hungry Knight</a></li>\
+                    </ul>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>\
+    </nav>'
